a576270ec9c691779479e267ea26657bc02d4313
makwerik <serezha0790@mail.ru>
2023-09-21 16:01:22 +0300

    Главный файл

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-# создается экземляр класса и передается __name__ сам главный файл в котром написан данный код
 app = Flask(__name__)
-# Добавляем бд, с которой будем работать
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-# Создаем экземляр бд
+
 db = SQLAlchemy(app)
 
 
-# Создание класса бд
 class Posts(db.Model):
     """
     Класс для ДБ с постами.
@@ -32,7 +30,6 @@ def home():
     """
     Функция главной страницы с оторажением данных из БД
     """
-    # query позволяет обратиться через опереденный класс к БД, которая соотвествует названию и отобразить посты на html
     show_posts = Posts.query.order_by(Posts.date).all()
     return render_template('home.html', show_posts=show_posts)
 
@@ -47,13 +44,11 @@ def create_post():
     """
     Функция добавляющая посты в БД
     """
-    # Если метод пост то идет добавление данных из формы по id в таблицу
     if request.method == 'POST':
         heading = request.form['heading']
         announcement = request.form['announcement']
         text = request.form['text']
         posts = Posts(heading=heading, announcement=announcement, text=text)
-        # Пытаемся сохранить данные в бд
         try:
             db.session.add(posts)
             db.session.commit()
@@ -92,7 +87,6 @@ def update_post(id):
     """
     Редактирование статьи в БД
     """
-# Если метод пост, то обращаемся к экземляру класса( тобишь к конткретной БД и меняем значение на полученные из формы
     update_p = Posts.query.get(id)
     if request.method == 'POST':
         update_p.heading = request.form['heading']
@@ -108,5 +102,4 @@ def update_post(id):
 
 
 if __name__ == '__main__':
-    # True , пока приложение не загружено на сервер
     app.run(debug=True)
